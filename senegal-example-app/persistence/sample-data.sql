ad706fab40853280db450e0e9edeaf4481c16f16
Jonathan Weiss <jonathan.weiss@ergon.ch>
2022-12-20 23:49:25 +0100

    Add some sample data for person

---
+++ senegal-example-app/persistence/sample-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO PERSON VALUES (uuid(), 'Peter', 'Klaus', 'Nelke');
+INSERT INTO PERSON VALUES (uuid(), 'Frida', '', 'Berington');
+INSERT INTO PERSON VALUES (uuid(), 'Analida', '', 'Heinzbach');
+INSERT INTO PERSON VALUES (uuid(), 'Hektor', '', 'Greenchen');
+Commit;
