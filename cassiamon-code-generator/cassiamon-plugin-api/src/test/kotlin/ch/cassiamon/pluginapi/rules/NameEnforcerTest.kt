608924a27d33059b4b9697227a5e330a8a7f1e46
Jonathan Weiss <jonathan.weiss@ergon.ch>
2023-02-15 08:24:26 +0100

    Fix lint issues

--- cassiamon-code-generator/cassiamon-plugin-api/src/test/kotlin/ch/cassiamon/pluginapi/rules/NameEnforcerTest.kt
+++ cassiamon-code-generator/cassiamon-plugin-api/src/test/kotlin/ch/cassiamon/pluginapi/rules/NameEnforcerTest.kt
@@ -1,39 +1,40 @@
 package ch.cassiamon.pluginapi.rules
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 
 internal class NameEnforcerTest {
 
     @org.junit.jupiter.api.Test
     fun isValidName() {
 
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_VALID, "ConceptName")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_VALID, "Concept123Name")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_VALID, "CONCEPT123NAME")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_VALID, "C1")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_VALID, "Ca")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_VALID, "CA")
+        assertThat(Validity.NAME_IS_VALID, "ConceptName")
+        assertThat(Validity.NAME_IS_VALID, "Concept123Name")
+        assertThat(Validity.NAME_IS_VALID, "CONCEPT123NAME")
+        assertThat(Validity.NAME_IS_VALID, "C1")
+        assertThat(Validity.NAME_IS_VALID, "Ca")
+        assertThat(Validity.NAME_IS_VALID, "CA")
 
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID, "")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID, "C")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID, "concept123Name")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID, " Concept123Name")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID, "cONCEPT123Name")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID, "_ConceptName")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID, "Concept_Name")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID, "Concept Name")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID, "Concept-Name")
-        assertThat(ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID, "-ConceptName")
+        assertThat(Validity.NAME_IS_NOT_VALID, "")
+        assertThat(Validity.NAME_IS_NOT_VALID, "C")
+        assertThat(Validity.NAME_IS_NOT_VALID, "concept123Name")
+        assertThat(Validity.NAME_IS_NOT_VALID, " Concept123Name")
+        assertThat(Validity.NAME_IS_NOT_VALID, "cONCEPT123Name")
+        assertThat(Validity.NAME_IS_NOT_VALID, "_ConceptName")
+        assertThat(Validity.NAME_IS_NOT_VALID, "Concept_Name")
+        assertThat(Validity.NAME_IS_NOT_VALID, "Concept Name")
+        assertThat(Validity.NAME_IS_NOT_VALID, "Concept-Name")
+        assertThat(Validity.NAME_IS_NOT_VALID, "-ConceptName")
     }
 
-    private fun assertThat(validity: ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity, name: String) {
+    private fun assertThat(validity: Validity, name: String) {
         when(validity) {
-            ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_VALID -> assertTrue(
-                ch.cassiamon.pluginapi.rules.NameEnforcer.isValidName(
+            Validity.NAME_IS_VALID -> assertTrue(
+                NameEnforcer.isValidName(
                     name
                 ), "Name '$name' was assert to be valid, but was not.")
-            ch.cassiamon.pluginapi.rules.NameEnforcerTest.Validity.NAME_IS_NOT_VALID -> assertFalse(
-                ch.cassiamon.pluginapi.rules.NameEnforcer.isValidName(
+            Validity.NAME_IS_NOT_VALID -> assertFalse(
+                NameEnforcer.isValidName(
                     name
                 ), "Name '$name' was assert to be invalid, but was valid.")
         }
