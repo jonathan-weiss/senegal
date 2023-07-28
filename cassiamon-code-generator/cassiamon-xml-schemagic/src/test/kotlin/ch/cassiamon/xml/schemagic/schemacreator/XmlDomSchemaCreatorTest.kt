5cbc31144cf2e29daa9293a0d59729f6783892aa
Jonathan Weiss <jonathan.weiss@ergon.ch>
2023-07-28 09:58:46 +0200

    Fix failing test by adding conceptIdentifier unique declaration

--- cassiamon-code-generator/cassiamon-xml-schemagic/src/test/kotlin/ch/cassiamon/xml/schemagic/schemacreator/XmlDomSchemaCreatorTest.kt
+++ cassiamon-code-generator/cassiamon-xml-schemagic/src/test/kotlin/ch/cassiamon/xml/schemagic/schemacreator/XmlDomSchemaCreatorTest.kt
@@ -15,6 +15,10 @@ internal class XmlDomSchemaCreatorTest {
     private val expectedXml = """
         <?xml version="1.0" encoding="UTF-8" standalone="no"?>
         <xsd:schema xmlns="https://cassiamon.ch/cassiamon-schemagic" elementFormDefault="qualified" targetNamespace="https://cassiamon.ch/cassiamon-schemagic" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+            <!-- - - - - - - - -       CONCEPT IDENTIFIER ATTRIBUTE     - - - - - - - -->
+            <xsd:attributeGroup name="conceptIdentifier">
+                <xsd:attribute name="conceptIdentifier" type="xsd:ID"/>
+            </xsd:attributeGroup>
             <!-- - - - - - - - -       CONFIGURATION AND DEFINITIONS     - - - - - - - -->
             <xsd:element name="cassiamon">
                 <xsd:complexType>
@@ -36,10 +40,12 @@ internal class XmlDomSchemaCreatorTest {
                 <xsd:choice maxOccurs="unbounded" minOccurs="0">
                     <xsd:element name="testEntityAttribute" type="testEntityAttributeType"/>
                 </xsd:choice>
+                <xsd:attributeGroup ref="conceptIdentifier"/>
                 <xsd:attributeGroup ref="testEntityName"/>
             </xsd:complexType>
             <xsd:complexType name="testEntityAttributeType">
                 <xsd:choice maxOccurs="unbounded" minOccurs="0"/>
+                <xsd:attributeGroup ref="conceptIdentifier"/>
                 <xsd:attributeGroup ref="testEntityAttributeName"/>
             </xsd:complexType>
             <!-- - - - - - - - -       ALL ATTRIBUTES      - - - - - - - -->
